871f91acab73cd7cb6e1690d16e847ee0c6102da
Code Sandbox <hello@codesandbox.io>
2022-05-05 07:00:00 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.922f38d285322fac87afcdc35dc026d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b9b266ce2de391baafad4ea974f6974",
+    "url": "https://weiyu-t.github.io/csb-v8opl6/index.html"
+  },
+  {
+    "revision": "5bac6f2ad2c095d2ae3f",
+    "url": "https://weiyu-t.github.io/csb-v8opl6/static/css/main.fbb427c6.chunk.css"
+  },
+  {
+    "revision": "8aed0a06c152013e40d6",
+    "url": "https://weiyu-t.github.io/csb-v8opl6/static/js/2.4234b8bd.chunk.js"
+  },
+  {
+    "revision": "5bac6f2ad2c095d2ae3f",
+    "url": "https://weiyu-t.github.io/csb-v8opl6/static/js/main.5d5e46ae.chunk.js"
+  },
+  {
+    "revision": "4703e704e08a066532a8",
+    "url": "https://weiyu-t.github.io/csb-v8opl6/static/js/runtime-main.5df5cba6.js"
+  }
+]);
